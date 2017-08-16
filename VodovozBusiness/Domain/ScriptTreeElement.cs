a5ade44c5e6f8f578721c38fcb5ed02933b388f0
ruSaendra <saendra.ru@gmail.com>
2017-08-16 11:41:43 +0300

    (I-618) Создан класс для элементов древа скрипта диалога.

---
+++ VodovozBusiness/Domain/ScriptTreeElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Gamma.Utilities;
+using NHibernate.Criterion;
+using QSBusinessCommon.Domain;
+using QSOrmProject;
+
+namespace Vodovoz.Domain
+{
+	public class ScriptTreeElement : PropertyChangedBase
+	{
+		string name;
+
+		public virtual string Name {
+			get { return name; }
+			set { SetField(ref name, value, () => Name); }
+		}
+
+		string text;
+
+		public virtual string Text {
+			get { return text; }
+			set { SetField(ref text, value, () => Text); }
+		}
+
+		string nextElementsUnparced;
+
+		public virtual string NextElementsUnparced {
+			get { return nextElementsUnparced; }
+			set { SetField(ref nextElementsUnparced, value, () => NextElementsUnparced); }
+		}
+
+		DialogueScriptWidget widget;
+
+		public virtual DialogueScriptWidget Widget {
+			get { return widget; }
+			set { SetField(ref widget, value, () => Widget); }
+		}
+
+		public virtual List<string> ParseNextElements()
+		{
+			return this.NextElementsUnparced.Split(',').ToList();
+		}
+
+		public virtual void SetNextElements(List<string> nextElementsParced)
+		{
+			this.NextElementsUnparced = string.Join(",", nextElementsParced.ToArray());
+		}
+	}
+
+	public enum DialogueScriptWidget
+	{
+		text,
+		nottext
+	}
+}
---
+++ VodovozBusiness/HibernateMapping/ScriptTreeElementMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using Vodovoz.Domain;
+
+namespace Vodovoz.HibernateMapping
+{
+	public class ScriptTreeElementMap : ClassMap<ScriptTreeElement>
+	{
+		public ScriptTreeElementMap()
+		{
+			Table("script_tree_elements");
+
+			Id(x => x.Name).Column("name");
+
+			Map(x => x.Text).Column("text");
+			Map(x => x.NextElementsUnparced).Column("next_element");
+			Map(x => x.Widget).Column("widget");
+		}
+	}
+}
