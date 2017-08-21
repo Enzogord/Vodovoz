027458e9733a46d19cdce42bc252b4d0d4ec1480
ruSaendra <saendra.ru@gmail.com>
2017-08-21 12:33:29 +0300

    (I-618) Добавлен виджет для текстового поля.

---
+++ Vodovoz/ViewWidgets/DialogueScriptWidgets/DialogueTextWidget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.DialogueScriptWidgets
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DialogueTextWidget : Gtk.Bin
+	{
+		public DialogueTextWidget()
+		{
+			this.Build();
+		}
+	}
+}
