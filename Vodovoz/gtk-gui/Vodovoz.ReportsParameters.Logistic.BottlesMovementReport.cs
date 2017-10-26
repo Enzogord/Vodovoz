5724bb995c08fc077bfade8e8aebeff2a7c340ca
Alex <all_reg@inbox.ru>
2017-10-26 14:45:55 +0300

    Восстановление диалогов после мержа3.

---
+++ Vodovoz/gtk-gui/Vodovoz.ReportsParameters.Logistic.BottlesMovementReport.cs
@@ -0,0 +1,20 @@
+
+namespace Vodovoz.ReportsParameters.Logistic
+{
+	public partial class BottlesMovementReport
+	{
+		private Gtk.VBox vbox1;
+
+		private Gtk.HBox hbox1;
+
+		private Gtk.Label label1;
+
+		private QSWidgetLib.DatePeriodPicker dateperiodpicker;
+
+		private Gtk.Button buttonCreateReport;
+
+		private void Build()
+		{
+		}
+	}
+}
---
+++ Vodovoz/gtk-gui/Vodovoz.ReportsParameters.Logistic.ShipmentReport.cs
@@ -0,0 +1,28 @@
+
+namespace Vodovoz.ReportsParameters.Logistic
+{
+	public partial class ShipmentReport
+	{
+		private Gtk.VBox vbox2;
+
+		private Gtk.HBox hbox1;
+
+		private Gtk.Label labelDate;
+
+		private Gamma.Widgets.yDatePicker ydatepicker;
+
+		private Gtk.RadioButton radioAll;
+
+		private Gtk.RadioButton radioCash;
+
+		private Gtk.RadioButton radioWarehouse;
+
+		private Gamma.Widgets.yEntryReference referenceWarehouse;
+
+		private Gtk.Button buttonCreateReport;
+
+		private void Build()
+		{
+		}
+	}
+}
